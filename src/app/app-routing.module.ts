ca774c8fb7334db5df4b1db74286d6cd829e179b
Arnaud Turcotte <arnaudt09@gmail.com>
2024-02-05 17:52:32 -0500

    PROGRESS : Ajout du bouton et du routerlink vers le component shop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MatchComponent } from './match/match.component';
+import {ShopComponent} from "./shop/shop.component";
 
 const routes: Routes = [
   { path: 'match/:id', component: MatchComponent },
   { path: '', component: HomeComponent, children: [
 
   ]},
+  { path: 'shop/', component: ShopComponent },
   { path: '**', redirectTo: '/'}
 ];
 
