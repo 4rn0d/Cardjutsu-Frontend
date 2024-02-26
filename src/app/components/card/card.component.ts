d95ddf63981c55a7367b29ce86ae95dda3470b0e
Arnaud Turcotte <arnaudt09@gmail.com>
2024-02-26 18:01:03 -0500

    Ajout du card-backside

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -11,7 +11,7 @@ export class CardComponent implements OnInit {
   @Input() card?:Card;
   @Input() show:string = "front";
   @Input() health:number = 0;
-  beautifulBackUrl = "https://i.pinimg.com/236x/3c/73/0d/3c730d6df70700a3c912a3c87d6d2027.jpg";
+  beautifulBackUrl = "https://static.miraheze.org/cpfanonwiki/f/f7/Formation_of_Water_Dojo.png";
 
   constructor() { }
 
